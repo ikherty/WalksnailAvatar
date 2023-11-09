5628f08d9b6e1094ed2bd067ab5835f4eea61cc1
ikherty <ikherty@users.noreply.github.com>
2023-11-09 03:26:45 +0000

    deploy: 4db2c5a68ab5010f29e8be979abd361cc95d9d7a

---
+++ assets/3D.md.8b4171b1.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as r,Q as a}from"./chunks/framework.edbfdbf1.js";const m=JSON.parse('{"title":"Файлы печати","description":"","frontmatter":{},"headers":[],"relativePath":"3D.md","filePath":"3D.md"}'),h={name:"3D.md"},n=a("",41),i=[n];function o(s,w,g,p,l,c){return t(),r("div",null,i)}const _=e(h,[["render",o]]);export{m as __pageData,_ as default};
